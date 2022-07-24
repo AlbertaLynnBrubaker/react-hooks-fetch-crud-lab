69996fdfd13c1e284f529cfa875de3ae269b133b
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-07-23 23:14:00 -0500

    commit tree 6cab9da3e22699948be5365872c90aacd265d8f5

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,49 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
 
+  const handleLoadList = (loadQuestions) => {
+    setQuestions(q => loadQuestions)
+  }
+
+  const handleAddQuestion = (newQuestion) => {
+    setQuestions(q => [
+      ...questions, newQuestion
+    ]
+    )
+  }
+
+  const handleDeleteQuestion = (deletedQuestion) => {
+    const updatedQuestions = questions.filter(question => deletedQuestion.id !== question.id)
+    setQuestions(() => updatedQuestions)
+  }
+
+  const handleUpdateQuestion = (updatedQuestion) => {
+    const updatedQuestions = questions.map(question => {
+      if(updatedQuestion.id === question.id) {
+        return updatedQuestion;
+      } else {
+        return question;
+      }
+    })
+
+    setQuestions(q => updatedQuestions)
+  }
+  
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? 
+        <QuestionForm 
+          onAddQuestion={handleAddQuestion} 
+        /> : 
+        <QuestionList 
+          questions={questions} 
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateQuestion={handleUpdateQuestion} 
+          onLoadList={handleLoadList} 
+      />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({onUpdateQuestion, onDeleteQuestion, questions, onLoadList}) {
+  
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then(r => r.json())
+      .then(loadQuestions => onLoadList(loadQuestions))
+
+    return () => {
+      onLoadList([])
+    }
+    
+  }, [])
+
+  const onDeleteQuestionItem = (deletedQuestion) => {
+    return onDeleteQuestion(deletedQuestion)
+  }
+
+  const onUpdateQuestionItem = (updatedQuestion) => {
+    return onUpdateQuestion(updatedQuestion)
+  }
+
+  const qsToDisplay = questions.map(question => {
+    return <QuestionItem 
+      key={question.id} 
+      onDeleteQuestionItem={onDeleteQuestionItem} 
+      onUpdateQuestionItem={onUpdateQuestionItem}
+      question={question}/>
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{qsToDisplay}</ul>
     </section>
   );
 }
